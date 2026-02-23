59b27960ecc41125ca7214adcf6039fa81d96d13
Andy <andersonlavor@gmail.com>
2026-02-23 05:28:58 -0600

    fix: allow publish script to proceed when nothing to commit or tag exists

--- script/publish.ts
+++ script/publish.ts
@@ -62,8 +62,8 @@ if (Script.release) {
   const tag = `v${Script.version}`
 
   if (!Script.preview) {
-    await $`git commit -am "release: v${Script.version}"`
-    await $`git tag v${Script.version}`
+    await $`git commit -am "release: v${Script.version}"`.nothrow()
+    await $`git tag v${Script.version}`.nothrow()
     await $`git fetch origin`
     await $`git cherry-pick HEAD..origin/dev`.nothrow()
     await $`git push origin HEAD --tags --no-verify --force-with-lease`
