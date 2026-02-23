fbdd8c17c60d696a091ed72c5f6797de1542df19
Andy <andersonlavor@gmail.com>
2026-02-22 23:39:58 -0600

    Replace remaining @opencode-ai/sdk imports with @openzero/sdk

--- script/changelog.ts
+++ script/changelog.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env bun
 
 import { $ } from "bun"
-import { createOpencode } from "@opencode-ai/sdk/v2"
+import { createOpencode } from "@openzero/sdk/v2"
 import { parseArgs } from "util"
-import { Script } from "@opencode-ai/script"
+import { Script } from "@openzero/script"
 
 type Release = {
   tag_name: string
